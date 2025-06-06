689f01f00ade477168fd83d8fb72dfc68935b993
alper <alpercanyadigar@gmail.com>
2025-06-07 01:35:17 +0300

    Fix api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000' + '/api'
+axios.defaults.baseURL = (import.meta.env.VITE_API_URL || 'http://localhost:3000') + '/api'
 
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
